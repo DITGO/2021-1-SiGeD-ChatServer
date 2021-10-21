1c6a99953ee655869f4d80cc9b9be619eed292d6
MoacirMSJ <moacirmascarenha86@gmail.com>
2021-10-21 14:08:51 -0300

    Adionada logica de salas.

--- index.js
+++ index.js
@@ -29,15 +29,34 @@ const generateRoomId = (id1,id2) => {
   return id2+id1;
 }
 
+const createRoomToUserIfNotExist = (userId) =>{
+  client.get(userId, (err, room) => {
+    if (err | room == null) {
+      client.set(userId, `{}`);
+    };
+  });
+}
+
+const updateMyRooms = (userId, roomId, status) => {
+  var myRooms;
+  client.get(userId, (err, reply) => {
+    if (err) throw err;
+    myRooms = JSON.parse(reply);
+    myRooms[`${roomId}`] = status;
+    client.set(userId, `${JSON.stringify(myRooms)}`);
+    console.log('conversas: ', myRooms);
+  });
+}
+
 io.on('connection', async(socket) => {
 
   //Ouvindo evento message
   socket.on('message', async(data)=>{
     
-
     const message = { ...data.data, 'date': new Date()};
     console.log("mensagem recebida", JSON.stringify(message));
     //Verificar se a conversar já existe
+
     var roomId = message.roomId;
     if(roomId === ''){
       roomId = generateRoomId(message.userId, message.toId);
@@ -46,9 +65,36 @@ io.on('connection', async(socket) => {
       client.set(roomId, `[${newList}]`);
       console.log(JSON.stringify(`[${message}]`));
     }
+    else{
+      // Pegar os dados da chave no redis e adicionar nova mensagem
+      var roomData = [];
+      client.get(roomId,(err, reply) => {
+        if (err) throw err;
+        roomData = JSON.parse(reply);
+        roomData.push(message);
+        //Adicionando nova mensagem a conversa
+        client.set(roomId, `${JSON.stringify(roomData)}`);
+        console.log('conversa: ', roomData);
+      });
+    }
 
-    console.log(JSON.stringify(message));
+    // Verificar se é a primeira Conversa do usuário
+    createRoomToUserIfNotExist(message.toId);
+    createRoomToUserIfNotExist(message.userId);
+    //Adiciona status das conversas em cada chave de usuário
+    updateMyRooms(message.userId, roomId, 0);
+    updateMyRooms(message.toId, roomId, 1);
 
+    //Notificar usuário sobre nova mensagem
+    io.emit(message.toId, {alert: roomId});
+    // console.log(JSON.stringify(message));
+    //Enviando emitindo mensagem para sala(conversa)
+    io.emit(roomId,message);
+  });
+  
+  
+  socket.on("disconnect", async () => {
+   console.log("Um usuário se desconectou");
   });
 
 });
@@ -60,7 +106,7 @@ app.get('/', (req, res) => {
       .status(200)
       .send('Hello, world!')
       .end();
-  });
+});
 
 app.use(bodyParser.json());
 
